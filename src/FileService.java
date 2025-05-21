31aa8db8361d0c068bf61b0f6bdf165375c7e945
Krissapter <kevenrud@hotmail.no>
2025-05-21 10:48:55 +0200

    Theoretically make file reading input into the database

--- src/FileService.java
+++ src/FileService.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class FileService {
     public void getMoviesFromFile(String filename) throws FileNotFoundException{
-        ArrayList<Movie> movies = new ArrayList<>();
+        DBService dbService = new DBService();
         File file = new File(filename);
         Scanner fs = new Scanner(file);
         while(fs.hasNext()){
@@ -14,10 +14,11 @@ public class FileService {
             movie.setMovieDesc(fs.nextLine());
             movie.setGenre(Genre.valueOf(fs.nextLine()));
             movie.setMovieRuntime(Integer.parseInt(fs.nextLine()));
-            movies.add(movie);
-        }
-        for (Movie m : movies){
-            System.out.println(m);
+            try{
+                dbService.insertMovie(movie);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
